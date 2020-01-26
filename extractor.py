dbd30e6b12bb38aa3f0a9e52f8157bfeae1acd2e
Aviatore <wojtek.wes@gmail.com>
2020-01-26 21:29:15 +0100

    New extractor file

--- extractor.py
+++ extractor.py
@@ -1,10 +1,39 @@
-import urllib.request as r
-import json
+import urllib.request
+from html.parser import HTMLParser
 
-with open('data.json', 'r') as f:
-    dataRaw = f.read()
 
-data = json.loads(dataRaw)
-for i in data['list']:
-    print("{} : {} : {}".format(i['weather'][0]['description'], i['weather'][0]['icon'], i['dt_txt']))
+class Parser(HTMLParser):
+    def __init__(self):
+        super().__init__()
+        self.data = {}
+        self.day = ""
+        self.c = 0
 
+    def pr(self):
+        for key in self.data.keys():
+            print(key, self.data[key])
+
+    def handle_starttag(self, tag, attrs):
+        if tag == "div":
+            for atr in attrs:
+                if atr[1] in ["day1", "day2", "day3"]:
+                    self.day = atr[1]
+                    self.c = 1
+
+        if tag == "img":
+            for atr in attrs:
+                if atr[1] == "weather_pictogram": c = 1
+                if self.c == 1 and atr[0] == "title":
+                    self.data[self.day] = atr[1]
+                    self.c = 0
+
+
+url = "https://www.meteoblue.com/pl/pogoda/tydzie%C5%84/krak%C3%B3w_polska_3094802"
+
+p = Parser()
+
+resp = urllib.request.urlopen(url)
+outputRaw = resp.read().decode('utf-8')
+
+p.feed(outputRaw)
+p.pr()
