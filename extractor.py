4b86139fef03b15b81540b6afce12428d85e8d6c
Wojciech Wesołowski <w.wesolowski@vp.pl>
2020-01-31 00:07:16 +0100

    Minor improvement to the script

--- extractor.py
+++ extractor.py
@@ -1,5 +1,6 @@
 import urllib.request
 from html.parser import HTMLParser
+import re
 
 
 class Parser(HTMLParser):
@@ -22,7 +23,7 @@ class Parser(HTMLParser):
 
         if tag == "img":
             for atr in attrs:
-                if atr[1] == "weather_pictogram": c = 1
+                if atr[1] == "weather_pictogram": self.c = 1
                 if self.c == 1 and atr[0] == "title":
                     self.data[self.day] = atr[1]
                     self.c = 0
@@ -33,28 +34,53 @@ class AGHParser(HTMLParser):
         super().__init__()
         self.data = {}
         self.parameters = ["Temperatura powietrza", "Ciśnienie atmosferyczne", "Prędkość wiatru", "pm 10"]
-        self.grabPrameter = 0
+        self.grabParameter = 0
         self.grabValue = 0
-        self.tmp = []
+        self.tmp = ["", 0, ""]
 
     def handle_starttag(self, tag, attrs):
         if tag == "td":
             self.grabParameter = 1
-        elif tag == "span"
+        elif tag == "span" and self.tmp[0] != "" and self.tmp[1] < 2:
+            self.tmp[1] += 1
+            if self.tmp[1] == 2:
+                self.grabValue = 1
+            
+
 
     def handle_data(self, data):
         if self.grabParameter and data in self.parameters:
-            self.tmp.append(data)
-        elif self.grabValue and self
+            self.tmp[0] = data
+            self.grabParameter = 0
+        elif self.grabValue:
+            self.tmp[2] = data.rstrip()
+            self.grabValue = 0
+            self.grabParameter = 0
+            r = re.compile(r'\s')
+            
+            tmp = r.split(self.tmp[2])
+            if tmp[-1] != "°C":
+                self.tmp[2] = tmp[0] + " " + tmp[-1]
+            else:
+                self.tmp[2] = tmp[0] + tmp[-1]
+
+            if self.tmp[0] == "pm 10":
+                self.tmp[2] += '3'
+
+            self.data[self.tmp[0]] = self.tmp[2]
+
+#            print("DEBUG", self.tmp[2], self.tmp[0])
+            self.tmp = ["", 0, ""]
+
 
 
 url_agh = "http://meteo.ftj.agh.edu.pl/main"
 url = "https://www.meteoblue.com/pl/pogoda/tydzie%C5%84/krak%C3%B3w_polska_3094802"
 
-p = Parser()
+p = AGHParser()
 
-resp = urllib.request.urlopen(url)
+resp = urllib.request.urlopen(url_agh)
 outputRaw = resp.read().decode('utf-8')
 
 p.feed(outputRaw)
-p.pr()
+print(p.data["pm 10"])
